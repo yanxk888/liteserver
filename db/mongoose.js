801237cc0dfa44cf37af1be2d4eb59b862e0bcdc
yanxk888 <yanxk888@163.com>
2020-04-03 00:20:33 +0800

    change database name from douyin to kuaishou

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/douyin')
+mongoose.connect('mongodb://localhost:27017/kuaishou')
 
 mongoose.connection.on('connected', function () { 
     console.log('Mongoose connection opened')
